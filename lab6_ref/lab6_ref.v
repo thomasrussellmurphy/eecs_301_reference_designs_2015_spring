dd15262d8ee3c5e9d9305d35e1d92501773aa696
Thomas Russell Murphy <thomas.russell.murphy@case.edu>
2015-03-12 14:04:11 -0400

    Creating Lab 5 and 6 project directories

---
+++ lab6_ref/lab6_ref.v
@@ -0,0 +1,48 @@
+module lab6_ref
+       (
+           //////////////////////// Clock Input ////////////////////////
+           input CLOCK_50,
+           input CLOCK_50_2,
+           //////////////////////// Push Button ////////////////////////
+           input [ 2: 0 ] BUTTON,
+           //////////////////////// DPDT Switch ////////////////////////
+           input [ 9: 0 ] SW,
+           //////////////////////// 7-SEG Display ////////////////////////
+           output [ 6: 0 ] HEX0_D,
+           output HEX0_DP,
+           output [ 6: 0 ] HEX1_D,
+           output HEX1_DP,
+           output [ 6: 0 ] HEX2_D,
+           output HEX2_DP,
+           output [ 6: 0 ] HEX3_D,
+           output HEX3_DP,
+           //////////////////////// LED ////////////////////////
+           output [ 9: 0 ] LEDG,
+           //////////////////////// GPIO ////////////////////////
+           input [ 1: 0 ] GPIO0_CLKIN,
+           output [ 1: 0 ] GPIO0_CLKOUT,
+           inout [ 31: 0 ] GPIO0_D,
+           input [ 1: 0 ] GPIO1_CLKIN,
+           output [ 1: 0 ] GPIO1_CLKOUT,
+           inout [ 31: 0 ] GPIO1_D
+       );
+
+// =======================================================
+// REG/WIRE declarations
+// =======================================================
+
+
+//=======================================================
+// Input/Output assignments
+//=======================================================
+// All unused inout port turn to tri-state
+assign GPIO0_D = 32'hzzzzzzzz;
+assign GPIO1_D = 32'hzzzzzzzz;
+
+
+// =======================================================
+// Structural coding
+// =======================================================
+
+
+endmodule
